bcd8d89de3d4a3afadd61c3c6a9b5ad9de6820c7
noahshen <Noah.Shen87@gmail.com>
2018-03-19 00:05:13 +0800

    save

--- src/main/kotlin/com/github/noahshen/panther/core/Block.kt
+++ src/main/kotlin/com/github/noahshen/panther/core/Block.kt
@@ -7,7 +7,7 @@ import org.spongycastle.util.encoders.Hex
 import java.math.BigInteger
 
 /**
- * 区块(Block)类，包含了区块高度(height)，上一个区块哈希值(parentHash)，旷工账户地址(coinBase)，交易列表(transactions)和时间戳(time)。
+ *
  */
 class Block(
         val version: Int,
